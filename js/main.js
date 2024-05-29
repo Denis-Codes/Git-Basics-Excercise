5154a7a1cac59f95a1e490d1f1c065e189697294
Denis Libin <dnslibin4@gmail.com>
2024-05-29 15:24:04 +0300

    Gitignore

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function onBallClick(elBall) {
     elBall.style.width = (elBall.offsetWidth + 50) + 'px';
     elBall.style.height = (elBall.offsetHeight + 50) + 'px';
     // elBall.innerText = elBall.style.height
-    if (parseInt(elBall.style.height) > 400) {
+    if (elBall.style.height > '400px') {
         elBall.style.width = '100px';
         elBall.style.height = '100px';
     }
