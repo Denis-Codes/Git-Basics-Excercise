a80af4d8828391258076fa8e1b847070eae45c95
Denis Libin <dnslibin4@gmail.com>
2024-05-29 16:11:34 +0300

    Randomize size

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict'
 
 function onBallClick(elBall) {
-    elBall.style.width = (elBall.offsetWidth + 50) + 'px';
-    elBall.style.height = (elBall.offsetHeight + 50) + 'px';
+    elBall.style.width = (elBall.offsetWidth + getRandomInt(20, 61)) + 'px'
+    elBall.style.height = (elBall.offsetHeight + getRandomInt(20, 61)) + 'px'
     // elBall.innerText = elBall.style.height
     if (elBall.style.height > '400px') {
-        elBall.style.width = '100px';
-        elBall.style.height = '100px';
+        elBall.style.width = '100px'
+        elBall.style.height = '100px'
     }
 }
