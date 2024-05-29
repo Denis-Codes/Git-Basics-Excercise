ac562e7b70f6f5d97bacb04b48160a5232652cd8
Denis Libin <dnslibin4@gmail.com>
2024-05-29 15:19:23 +0300

    Add js only

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@
 function onBallClick(elBall) {
     elBall.style.width = (elBall.offsetWidth + 50) + 'px';
     elBall.style.height = (elBall.offsetHeight + 50) + 'px';
+    // elBall.innerText = elBall.style.height
+    if (parseInt(elBall.style.height) > 400) {
+        elBall.style.width = '100px';
+        elBall.style.height = '100px';
+    }
 }
